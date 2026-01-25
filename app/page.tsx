0d21fc33d1053e914591db3f84a127c2c5d799d6
Tama Lockridge <deskchatwithnick@gmail.com>
2026-01-24 21:58:01 -0500

    initial Zenavant web setup

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="min-h-screen flex items-center justify-center">
+      <h1 className="text-4xl font-semibold">
+        Zenavant
+      </h1>
+    </main>
+  );
+}
