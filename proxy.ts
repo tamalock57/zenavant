8e1acf702bf1d6ae303212283c225d3fe3c62e48
Tama Lockridge <deskchatwithnick@gmail.com>
2026-04-27 10:10:25 -0400

    rename middleware to proxy

--- proxy.ts
+++ proxy.ts
@@ -1,7 +1,7 @@
 import { NextResponse, type NextRequest } from "next/server";
 import { createServerClient } from "@supabase/ssr";
 
-export async function middleware(req: NextRequest) {
+export async function proxy(req: NextRequest) {
   let res = NextResponse.next();
 
   const supabase = createServerClient(
